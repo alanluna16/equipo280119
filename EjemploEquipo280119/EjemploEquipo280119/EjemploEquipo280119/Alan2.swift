73b5e6b3cb1b65e2cbd8ba6af487e3b98db014f8
alanluna16 <alanlunaa109@gmail.com>
2019-01-28 08:52:32 -0600

    commitalan2
    yes

---
+++ EjemploEquipo280119/EjemploEquipo280119/EjemploEquipo280119/Alan2.swift
@@ -0,0 +1,13 @@
+//
+//  Alan2.swift
+//  EjemploEquipo280119
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 28/01/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import UIKit
+
+class Alan2: NSObject {
+
+}
